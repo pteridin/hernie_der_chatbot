be3a866e978f61ffb82824e15f80f852464d04c5
Tobias Heidler <tobias.heidler@wig2.de>
2023-09-07 13:30:05 +0200

    Initial prompt success

--- mockup.py
+++ mockup.py
@@ -3,9 +3,33 @@ import openai
 
 openai.api_key = "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"
 
+prompt_primer = """
+Du bist ein AI-Assistent, der Patienten bei der Behandlung ihrer Erkrankung unterstützt.
+Die Nutzer können Fragen zu der folgenden Erkrankung stellen und Antworten erhalten: Leistenbruch.
+
+Dabei werden dir die folgenden Informationen zur Verfügung gestellt:
+
+```
+Als Leistenbruch bezeichnet man einen Bereich im Bauch bei dem die Bauchdecke nicht mehr intakt ist. 
+Diese Lücke kann sich weiter ausdehnen und dazu führen, dass Bauchorgane - meist Darmanteile -
+durch die Lücke hinausragen können und schlimmstenfalls absterben können. Hauptsymptom eines 
+Leistenbruches sind Schmerzen in der Leiste.
+
+Die Zahl ist 101.
+```
+
+Versuche dich anhand der Informationen in die Lage eines Patienten zu versetzen und beantworte die Fragen
+des Patienten. Du kannst auch weitere Fragen stellen, um die Situation des Patienten besser zu verstehen.
+Versuche dich jedoch sehr stark an den Informationen zu orientieren, die dir zur Verfügung gestellt werden.
+Und versuche, wenn möglich, ambulante Behandlungen zu empfehlen.
+"""
+
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+st.session_state.messages.append({"role": "system", "content": prompt_primer})
 
 # Write intro
 with st.chat_message("assistant"):
@@ -24,8 +48,9 @@ Oder stelle einfach eine Frage, wie 'Kann meine Erkrankung auch ambulant behande
     
 # handle chat history
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if(message["role"] != "system"):
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 
 prompt = st.chat_input("Frage etwas")
